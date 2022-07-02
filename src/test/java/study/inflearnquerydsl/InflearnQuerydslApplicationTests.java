29b4831fe78b3d75889d767d6fe43f46ec129d6f
pangnem <sprnd645@gmail.com>
2022-07-02 22:00:04 +0900

    스프링부트 설정 - JPA, DB

--- src/test/java/study/inflearnquerydsl/InflearnQuerydslApplicationTests.java
+++ src/test/java/study/inflearnquerydsl/InflearnQuerydslApplicationTests.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.inflearnquerydsl.entity.Hello;
 import study.inflearnquerydsl.entity.QHello;
