3ddee8a18000fddd04640853d63c1b875652da57
pangnem <sprnd645@gmail.com>
2022-07-02 20:57:07 +0900

    Initial Commit

---
+++ src/test/java/study/inflearnquerydsl/InflearnQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package study.inflearnquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
